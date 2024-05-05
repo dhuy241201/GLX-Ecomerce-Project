d8d93a1374e9bab454141979a22046c917465529
dhuy241201 <d.huy241201@gmail.com>
2024-05-05 20:58:10 +0700

    chore: create service project

---
+++ GLX Tech Service/src/main/java/com/service/glx/GlxApplication.java
@@ -0,0 +1,13 @@
+package com.service.glx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlxApplication.class, args);
+	}
+
+}
---
+++ GLX Tech Service/src/test/java/com/service/glx/GlxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.glx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
